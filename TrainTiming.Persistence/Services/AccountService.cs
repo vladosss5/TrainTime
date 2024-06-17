f65f82a22e11614070d6e1dac5c95a02ca7db18d
vladosss5 <kiporovv1@gmail.com>
2024-06-18 00:51:44 +0700

    save

---
+++ TrainTiming.Persistence/Services/AccountService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using TrainTiming.Application.Interfaces.IServices;
+
+namespace TrainTiming.Persistence.Services;
+
+public class AccountService : IAccountService
+{
+    static HttpClient client = new HttpClient();
+    
+    private readonly IConfiguration _configuration;
+    
+    public AccountService(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+    
+    public async Task<string> LoginAsync(string username, string password)
+    {
+        throw new NotImplementedException();
+    }
+}
